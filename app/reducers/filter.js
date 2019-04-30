ebdd627d26accf37ddaa114d77c25e2f33bcea92
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-04-29 21:39:52 -0300

    filter reducer finished

--- app/reducers/filter.js
+++ app/reducers/filter.js
@@ -1,7 +1,9 @@
 import INITIAL_STATE from '../actions/index';
-
+import FILTER_ACTIONS from '../actions/index'
 const filterReducer = (state = INITIAL_STATE.filter, action) =>{
     switch (action.type) {
+        case FILTER_ACTIONS.CHANGE_FILTER:
+            return action.filter;
         default:
             return state;
     }
