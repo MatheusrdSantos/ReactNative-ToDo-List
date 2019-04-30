f612806d1ef5b1358e5c1793aded3c2948235848
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-04-29 21:30:43 -0300

    new actions created on index file

--- app/actions/index.js
+++ app/actions/index.js
@@ -4,6 +4,15 @@ export const FILTERS = {
     INCOMPLETE: 'INCOMPLETE'
 }
 
+export const TASK_ACTIONS = {
+    NEW_TASK: 'NEW_TASK',
+    TOGGLE_STATUS: 'TOGGLE_STATUS'
+}
+
+export const FILTER_ACTIONS = {
+    CHANGE_FILTER: 'CHANGE_STATUS'
+}
+
 export const INITIAL_STATE = {
     filter: FILTERS.ALL,
     tasks: [
@@ -13,3 +22,15 @@ export const INITIAL_STATE = {
         }
     ],
 };
+
+export function newTask(description){
+    return {type: TASK_ACTIONS.NEW_TASK, description};
+}
+
+export function toggleTask(index){
+    return {type: TASK_ACTIONS.TOGGLE_STATUS, index};
+}
+
+export function changeFilter(filter){
+    return {type: FILTER_ACTIONS.CHANGE_FILTER, filter};
+}
