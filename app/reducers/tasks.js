8eaf79c5603850ee3d2441611640e32f09c70a6e
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-04-29 21:40:05 -0300

    task reducer finished

--- app/reducers/tasks.js
+++ app/reducers/tasks.js
@@ -1,9 +1,20 @@
 import INITIAL_STATE from '../actions/index';
-
+import TASK_ACTIONS from '../actions/index';
 const taskReducer = (state = INITIAL_STATE.tasks, action) => {
     switch (action.type) {
+      case TASK_ACTIONS.NEW_TASK:
+        return [...state, {text: action.text, done: false}];
+      case TASK_ACTIONS.TOGGLE_STATUS:
+        return state.map((task, index) => {
+          if (index === action.index) {
+            return Object.assign({}, task, {
+              done: !task.done
+            })
+          }
+          return task
+        })
       default:
-        return state
+        return state;
     }
 };
 
